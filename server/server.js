98a62ccd8376965629c17137625bc9344f134259
Matthieu Resurreccion <mattres2001@Matthieus-MacBook-Air-7.local>
2026-03-06 14:18:10 -0800

    switching to vercel and and inngest

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js'
-import { handleClerkWebhook } from './routes/webhookClerk.js';
+import { inngest, functions } from './inngest/index.js';
+// import { handleClerkWebhook } from './routes/webhookClerk.js';
 
 const app = express();
 
 await connectDB();
 
-
-app.post("/webhooks/clerk", express.raw({ type: "application/json" }), handleClerkWebhook);
+// app.post("/webhooks/clerk", express.raw({ type: "application/json" }), handleClerkWebhook);
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send("Server is running"));
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
 
